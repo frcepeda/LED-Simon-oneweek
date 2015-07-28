bdb367606fe18a9e377657d8878556ceb4a9406c
James Decker <t-jdeck@microsoft.com>
2015-07-28 12:56:18 -0700

    Adding LED Strip shit

---
+++ SimonSaysSOL/SimonSaysSOL/LEDControl/LEDStrip.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.Maker.Firmata;
+using Microsoft.Maker.RemoteWiring;
+using Microsoft.Maker.Serial;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.UI.Xaml;
+
+namespace SimonSaysSOL
+{
+    public class LEDStrip
+    {
+        private const int NEOPIXEL_SET_COMMAND = 0x42;
+        private const int NEOPIXEL_SHOW_COMMAND = 0x44;
+        private const int NUMBER_OF_PIXELS = 30;
+        private static volatile UwpFirmata firmata;
+        private static DispatcherTimer timeout;
+        private static IStream connection;
+
+        private static bool _init = false;
+        public static async void Init()
+        {
+            var deviceList = await UsbSerial.listAvailableDevicesAsync();
+            var device = deviceList.First();
+            connection = new UsbSerial(device);
+            firmata = new UwpFirmata();
+            firmata.begin(connection);
+            App.Arduino = new RemoteDevice(firmata);
+
+            connection.ConnectionEstablished += OnConnectionEstablished;
+            connection.ConnectionFailed += OnConnectionFailed;
+            connection.begin(115200, SerialConfig.SERIAL_8N1);
+
+            //start a timer for connection timeout
+            timeout = new DispatcherTimer();
+            timeout.Interval = new TimeSpan(0, 0, 30);
+            timeout.Tick += Connection_TimeOut;
+            timeout.Start();
+        }
+
+        /// <summary>
+        /// Sets all the pixels to the given color values and calls UpdateStrip() to tell the NeoPixel library to show the set colors.
+        /// </summary>
+        /// <param name="red"></param>
+        /// <param name="green"></param>
+        /// <param name="blue"></param>
+        public static void SetAllPixelsAndUpdate(byte red, byte green, byte blue)
+        {
+            SetAllPixels(red, green, blue);
+            UpdateStrip();
+        }
+
+        /// <summary>
+        /// Sets all the pixels to the given color values
+        /// </summary>
+        /// <param name="red">The amount of red to set</param>
+        /// <param name="green">The amount of green to set</param>
+        /// <param name="blue">The amount of blue to set</param>
+        public static void SetAllPixels(byte red, byte green, byte blue)
+        {
+            for (byte i = 0; i < NUMBER_OF_PIXELS; ++i)
+            {
+                SetPixel(i, red, green, blue);
+            }
+        }
+        public class RGB
+        {
+            public byte red;
+            public byte green;
+            public byte blue;
+        }
+
+        public static void SetPixel(int pixel, RGB color)
+        {
+            SetPixel((byte)pixel, color.red, color.green, color.blue);
+        }
+
+        public static void CrissCross(RGB first, RGB second)
+        {
+            
+        }
+
+        /// <summary>
+        /// Sets a single pixel to the given color values
+        /// </summary>
+        /// <param name="red">The amount of red to set</param>
+        /// <param name="green">The amount of green to set</param>
+        /// <param name="blue">The amount of blue to set</param>
+        private static void SetPixel(byte pixel, byte red, byte green, byte blue)
+        {
+            firmata.beginSysex(NEOPIXEL_SET_COMMAND);
+            firmata.appendSysex(pixel);
+            firmata.appendSysex(red);
+            firmata.appendSysex(green);
+            firmata.appendSysex(blue);
+            firmata.endSysex();
+        }
+
+        /// <summary>
+        /// Tells the NeoPixel strip to update its displayed colors.
+        /// This function must be called before any colors set to pixels will be displayed.
+        /// </summary>
+        /// <param name="red">The amount of red to set</param>
+        /// <param name="green">The amount of green to set</param>
+        /// <param name="blue">The amount of blue to set</param>
+        public static void UpdateStrip()
+        {
+            firmata.beginSysex(NEOPIXEL_SHOW_COMMAND);
+            firmata.endSysex();
+        }
+
+        public static void FREAKOUT(int freakCount)
+        {
+            Random r = new Random();
+            for (int j = 0; j < freakCount; j++)
+            {
+                for (byte i = 0; i < NUMBER_OF_PIXELS; i++)
+                {
+                    SetPixel(i, (byte)r.Next(256), (byte)r.Next(256), (byte)r.Next(256));
+                }
+                UpdateStrip();
+            }
+        }
+
+        public static void Clear()
+        {
+            SetAllPixelsAndUpdate(0,0,0);
+        }
+
+        /****************************************************************
+         *                  Event callbacks                             *
+         ****************************************************************/
+
+        private static void OnConnectionFailed(string message)
+        {
+            timeout.Stop();
+            Debug.WriteLine("ERROR: unable to connect!!");
+        }
+
+        private static void OnConnectionEstablished()
+        {
+            timeout.Stop();
+            Debug.WriteLine("Success: Connection Established");
+        }
+
+        private static void Connection_TimeOut(object sender, object e)
+        {
+            Debug.WriteLine("ERROR: Connection TImed Out");
+        }
+    }
+}
---
+++ SimonSaysSOL/SimonSaysSOL/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Maker.Firmata;
+using System;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.Maker.Serial;
+using Microsoft.Maker.RemoteWiring;
+using System.Diagnostics;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SimonSaysSOL
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        /// <summary>
+        /// This page uses advanced features of the Windows Remote Arduino library to carry out custom commands which are
+        /// defined in the NeoPixel_StandardFirmata.ino sketch. This is a customization of the StandardFirmata sketch which
+        /// implements the Firmata protocol. The customization defines the behaviors of the custom commands invoked by this page.
+        /// 
+        /// To learn more about Windows Remote Arduino, refer to the GitHub page at: https://github.com/ms-iot/remote-wiring/
+        /// To learn more about advanced behaviors of WRA and how to define your own custom commands, refer to the
+        /// advanced documentation here: https://github.com/ms-iot/remote-wiring/blob/develop/advanced.md
+        /// </summary>
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            LEDStrip.Init();
+        }
+
+        /// <summary>
+        /// This button callback is invoked when the buttons are pressed on the UI. It determines which
+        /// button is pressed and sets the LEDs appropriately
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void FlipOut_Click(object sender, RoutedEventArgs e)
+        {
+            LEDStrip.FREAKOUT(200);
+             //LEDStrip.SetAllPixelsAndUpdate((byte)r.Next(256), (byte)r.Next(256), (byte)r.Next(256));
+            //await Task.Delay(100);
+        }
+
+        private async void CrissCross_Click(object sender, RoutedEventArgs e)
+        {
+            //LEDStrip.CrissCross();
+        }
+
+        private void Off_Click(object sender, RoutedEventArgs e)
+        {
+            LEDStrip.Clear();
+        }
+    }
+}
